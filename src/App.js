44284f8c151f2c8b650a8167c7c56da59f305073
20120619 <mtuong669@gmail.com>
2022-11-15 21:10:12 +0700

    Set up layout and classname for project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from './routes';
+import { DefaultLayout } from './layouts';
 
 function App() {
     return (
@@ -8,7 +9,18 @@ function App() {
                 <Routes>
                     {publicRoutes.map((route, index) => {
                         const Page = route.component;
-                        return <Route key={index} path={route.path} element={<Page />} />;
+                        const Layout = route.layout || DefaultLayout;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
                     })}
                 </Routes>
             </div>
