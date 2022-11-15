244bc294abf3accc4ae4259a93cb6f47719c2fad
20120612 <nguyenlamtruong20021@gmail.com>
2022-11-15 22:29:16 +0700

    add headeronly layout and create sign page

---
+++ src/pages/Sign/index.js
@@ -0,0 +1,27 @@
+import styles from './Sign.module.scss';
+import classNames from 'classnames/bind';
+
+import { useState } from 'react';
+
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
+const cx = classNames.bind(styles);
+
+function Sign() {
+    const [isSignIn, setIsSignIn] = useState(true);
+
+    const handleChangeSign = () => {
+        setIsSignIn(!isSignIn);
+    };
+
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('slide')}>
+            </div>
+            {isSignIn ? <SignIn handleChangeSign={handleChangeSign} /> : <SignUp handleChangeSign={handleChangeSign} />}
+        </div>
+    );
+}
+
+export default Sign;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
+import { HeaderOnly } from '../layouts';
 import Home from '../pages/Home';
 import Sales from '../pages/Sales';
+import Sign from '../pages/Sign'
 
 const publicRoutes = [
     { path: '/', component: Home },
-    { path: '/sales', component: Sales },
+    { path: '/sales', component: Sales }, 
+    { path: '/sign', component: Sign, layout: HeaderOnly},
 ];
 
 const privateRoutes = [];
