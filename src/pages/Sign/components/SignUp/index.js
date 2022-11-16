10f5d5379c59ef6b19140e7abda268f62712160d
20120612 <nguyenlamtruong20021@gmail.com>
2022-11-16 22:23:45 +0700

    giao dien co ban cua form login & register

--- src/pages/Sign/components/SignUp/index.js
+++ src/pages/Sign/components/SignUp/index.js
@@ -1,5 +1,116 @@
-function SignUp() {
-    return <div>SignUp</div>;
+import styles from './SignUp.module.scss';
+import classNames from 'classnames/bind';
+
+import { useRef } from 'react';
+
+import { useForm } from 'react-hook-form';
+
+const cx = classNames.bind(styles);
+
+function SignUp({ handleChangeSign }) {
+    const {
+        register,
+        watch,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const password = useRef({});
+    password.current = watch('password', '');
+
+    const onSubmit = (data) => {
+        const { passwordRe, ...user } = data;
+        console.log(user);
+    };
+
+    return (
+        <div className={cx('login')}>
+            <div className={cx('wrapper')}>
+                <div className={cx('title')}>
+                    <h2>ĐĂNG KÝ</h2>
+                </div>
+                <div className={cx('content')}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        {/* <label>Họ tên</label> */}
+                        <div className={cx('input-error')}>
+                            <input placeholder="Họ và tên" {...register('name', { required: true })} />
+                            <p>{errors.name?.type === 'required' ? 'Không được bỏ trống' : ''}</p>
+                        </div>
+
+                        {/* <label>Tài khoản</label> */}
+                        <div className={cx('input-error')}>
+                            <input
+                                placeholder="Tài khoản"
+                                {...register('username', { required: true, minLength: 6 })}
+                            />
+                            <p>
+                                {errors.username?.type === 'required'
+                                    ? 'Không được bỏ trống'
+                                    : errors.username?.type === 'minLength'
+                                    ? 'Tài khoản phải có từ 6 kí tự'
+                                    : ''}
+                            </p>
+                        </div>
+                        {/* <label>Email</label> */}
+                        <div className={cx('input-error')}>
+                            <input
+                                placeholder="Nhập email"
+                                {...register('email', {
+                                    required: true,
+                                    minLength: 6,
+                                    pattern: {
+                                        value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                        message: 'invalid email address',
+                                    },
+                                })}
+                            />
+                            <p>
+                                {errors.email?.type === 'required'
+                                    ? 'Không được bỏ trống'
+                                    : errors.email?.type === 'minLength'
+                                    ? 'Email phải có từ 6 kí tự'
+                                    : errors.email?.type === 'pattern'
+                                    ? 'Nhập sai định dạng email!'
+                                    : ''}
+                            </p>
+                        </div>
+                        {/* <label>Mật khẩu</label> */}
+                        <div className={cx('input-error')}>
+                            <input
+                                type="password"
+                                placeholder="Nhập mật khẩu"
+                                {...register('password', { required: true, minLength: 6 })}
+                            />
+                            <p>
+                                {errors.password?.type === 'required'
+                                    ? 'Không được bỏ trống'
+                                    : errors.password?.type === 'minLength'
+                                    ? 'Mật khẩu phải có từ 6 kí tự'
+                                    : ''}
+                            </p>
+                        </div>
+                        {/* <label>Nhập lại mật khẩu</label> */}
+                        <div className={cx('input-error')}>
+                            <input
+                                type="password"
+                                placeholder="Nhập lại mật khẩu"
+                                {...register('passwordRe', {
+                                    validate: (value) => value === password.current,
+                                })}
+                            />
+                            <p>{errors.passwordRe?.type === 'validate' ? 'Mật khẩu không khớp' : ''}</p>
+                        </div>
+                        <button className={cx('submit')} type="submit">
+                            ĐĂNG KÝ
+                        </button>
+                        <button onClick={handleChangeSign} className={cx('signup')}>
+                            Bạn đã có tài khoản?
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default SignUp;
--- src/pages/Sign/index.js
+++ src/pages/Sign/index.js
@@ -1,15 +1,39 @@
 import styles from './Sign.module.scss';
 import classNames from 'classnames/bind';
 
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
+import canteen1 from './img/canteen1.jpg';
+import canteen2 from './img/canteen2.jpg';
+import canteen3 from './img/canteen3.jpg';
+
 const cx = classNames.bind(styles);
+
+const second = 3;
+const listSlideImg = [canteen1, canteen2, canteen3];
 
 function Sign() {
     const [isSignIn, setIsSignIn] = useState(true);
+
+    const [slideImg, setSlideImg] = useState(0);
+
+    const imgRef = useRef();
+
+    useEffect(() => {
+        let timer1;
+        if(slideImg === listSlideImg.length - 1) {
+            timer1 = setTimeout(() => setSlideImg(0), second * 1000);
+        } else {
+            timer1 = setTimeout(() => setSlideImg(slideImg + 1), second * 1000);
+        }
+
+        return () => {
+            clearTimeout(timer1);
+        };
+    }, [slideImg]);
 
     const handleChangeSign = () => {
         setIsSignIn(!isSignIn);
@@ -18,8 +42,15 @@ function Sign() {
     return (
         <div className={cx('wrapper')}>
             <div className={cx('slide')}>
+                <img ref={imgRef} className={cx('img')} src={listSlideImg[slideImg]} alt="img" />
             </div>
-            {isSignIn ? <SignIn handleChangeSign={handleChangeSign} /> : <SignUp handleChangeSign={handleChangeSign} />}
+            <div className={cx('sign')}>
+                {isSignIn ? (
+                    <SignIn handleChangeSign={handleChangeSign} />
+                ) : (
+                    <SignUp handleChangeSign={handleChangeSign} />
+                )}
+            </div>
         </div>
     );
 }
