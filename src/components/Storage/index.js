12285bbe6b4e7695a97123b75623252aed789b50
20120541 <nhiyenphan99@gmail.com>
2022-11-18 19:38:59 +0700

    storage

--- src/components/Storage/index.js
+++ src/components/Storage/index.js
@@ -1,57 +1,114 @@
 import React, { useState, useEffect } from 'react';
-import { BsUpload } from 'react-icons/bs';
+import { ref, getDownloadURL, uploadBytesResumable } from 'firebase/storage';
+import { collection, onSnapshot, addDoc } from 'firebase/firestore';
+import { storage, db } from '../../firebase/config';
 import classNames from 'classnames/bind';
 import styles from './Storage.module.scss';
 const cx = classNames.bind(styles);
 const Storage = () => {
     const [storages, setStorage] = useState([]);
+    const [img, setImg] = useState(null);
+    const [imgUrl, setImgUrl] = useState();
+    const [foodName, setFoodName] = useState('');
+    const [foodPrice, setFoodPrice] = useState('');
+    const [foodAmount, setFoodAmount] = useState('');
+    const [foodPriceImport, setFoodPriceImport] = useState('');
+
+    //read data
     useEffect(() => {
-        setStorage([
-            {
-                name: 'Phở bò',
-                price: 30000,
-                cost: 20000,
-                image: '/images/food1.png',
-                amount: 1,
+        const unSubscribe = onSnapshot(
+            collection(db, 'storage'),
+            (snapShot) => {
+                let list = [];
+                snapShot.docs.forEach((doc) => {
+                    list.push({ id: doc.id, ...doc.data() });
+                });
+                setStorage(list);
             },
-            {
-                name: 'Coca cola',
-                price: 10000,
-                cost: 20000,
-                image: '/images/food6.jpg',
-                amount: 1,
+            (error) => {
+                console.log(error);
             },
-            {
-                name: 'Coca cola',
-                price: 10000,
-                cost: 20000,
-                image: '/images/food6.jpg',
-                amount: 1,
-            },
-            {
-                name: 'Coca cola',
-                price: 10000,
-                cost: 20000,
-                image: '/images/food6.jpg',
-                amount: 1,
-            },
-            {
-                name: 'Coca cola',
-                price: 10000,
-                cost: 20000,
-                image: '/images/food6.jpg',
-                amount: 1,
-            },
-            {
-                name: 'Coca cola',
-                price: 10000,
-                cost: 20000,
-                image: '/images/food6.jpg',
-                amount: 1,
-            },
-        ]);
+        );
+
+        return () => {
+            unSubscribe();
+        };
     }, []);
-    //end test
+
+    //write data
+    useEffect(() => {
+        const uploadFile = () => {
+            const storageRef = ref(storage, img.name);
+            const uploadTask = uploadBytesResumable(storageRef, img);
+
+            uploadTask.on(
+                (error) => {
+                    console.log(error);
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        setImgUrl(downloadURL);
+                        console.log(downloadURL);
+                    });
+                },
+            );
+        };
+        img && uploadFile();
+    }, [img]);
+
+    //set img
+    useEffect(() => {
+        return () => {
+            img && URL.revokeObjectURL(img.preview);
+        };
+    }, [img]);
+
+    // const uploadImg = () => {
+    //     if (img === null) return;
+    //     const imgRef = ref(storage, `images/${img.name}`);
+    //     uploadBytes(imgRef, img).then((snapshot) => {
+    //         getDownloadURL(snapshot.ref).then((url) => {
+    //             setImgUrl(url);
+    //         });
+    //     });
+    // };
+
+    const handlePreviewImage = (e) => {
+        const imagePath = e.target.files[0];
+        imagePath.preview = URL.createObjectURL(imagePath);
+        setImg(imagePath);
+    };
+
+    const handleAdditem = async (e) => {
+        e.preventDefault();
+        const data = {
+            amount: parseInt(foodAmount),
+            img: imgUrl,
+            name: foodName,
+            price: parseInt(foodPrice),
+            priceImport: parseInt(foodPriceImport),
+            status: true,
+            typeFood: 'fast food',
+        };
+
+        try {
+            addDoc(collection(db, 'storage'), data);
+        } catch (err) {
+            console.log(err);
+        }
+        setFoodName('');
+        setFoodAmount('');
+        setFoodPrice('');
+        setFoodPriceImport('');
+    };
+
+    const getFood = (type, value) => {
+        if (type === 'name') setFoodName(value);
+        else if (type === 'amount') {
+            setFoodAmount(value);
+        } else if (type === 'price') setFoodPrice(value);
+        else if (type === 'import') setFoodPriceImport(value);
+    };
 
     return (
         <div className={cx('storage')}>
@@ -63,14 +120,14 @@ const Storage = () => {
                     <div className={cx('storage-list')}>
                         {storages.map((item, index) => (
                             <div className={cx('storage-item')} key={index}>
-                                <img src={item.image} alt="" />
+                                <img src={item.img} alt="" />
                                 <div className={cx('storage-info')}>
                                     <span>{item.name}</span>
                                     <span>Số lượng: {item.amount}</span>
                                 </div>
                                 <div className={cx('storage-price')}>
                                     <div className={cx('storage-price-total')}> Giá bán: {item.price}đ</div>
-                                    <div className={cx('storage-price-total')}> Giá nhập: {item.cost}đ</div>
+                                    <div className={cx('storage-price-total')}> Giá nhập: {item.priceImport}đ</div>
                                 </div>
                             </div>
                         ))}
@@ -79,16 +136,43 @@ const Storage = () => {
                 <div className={cx('storage-new-food')}>
                     <h4>Thêm sản phẩm</h4>
                     <div className={cx('storage-new-image')}>
-                        <img src="/images/food1.png" alt="" />
-                        <BsUpload className={cx('storage-new-upload')} />
+                        <img src={img === null ? '/images/img_upload.png' : img.preview} alt="" />
+                        <input
+                            type="file"
+                            onChange={(e) => {
+                                handlePreviewImage(e);
+                            }}
+                        />
                     </div>
                     <div className={cx('storage-info-food')}>
-                        <input className={cx('storage-food-name')} placeholder="Tên món ăn" />
-                        <input className={cx('storage-food-amount')} placeholder="Số lượng" />
-                        <input className={cx('storage-food-cost')} placeholder="Giá bán" />
-                        <input className={cx('storage-food-price')} placeholder="Giá nhập" />
+                        <input
+                            className={cx('storage-food-name')}
+                            placeholder="Tên món ăn"
+                            value={foodName}
+                            onChange={(e) => getFood('name', e.target.value)}
+                        />
+                        <input
+                            className={cx('storage-food-amount')}
+                            placeholder="Số lượng"
+                            value={foodAmount}
+                            onChange={(e) => getFood('amount', e.target.value)}
+                        />
+                        <input
+                            className={cx('storage-food-cost')}
+                            placeholder="Giá bán"
+                            value={foodPrice}
+                            onChange={(e) => getFood('price', e.target.value)}
+                        />
+                        <input
+                            className={cx('storage-food-price')}
+                            placeholder="Giá nhập"
+                            value={foodPriceImport}
+                            onChange={(e) => getFood('import', e.target.value)}
+                        />
                     </div>
-                    <button className={cx('storage-btn-add')}>Thêm sản phẩm</button>
+                    <button onClick={(e) => handleAdditem(e)} className={cx('storage-btn-add')}>
+                        Thêm sản phẩm
+                    </button>
                 </div>
             </div>
         </div>
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,6 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -24,5 +25,5 @@ const analytics = getAnalytics(app);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
-
-export { db, auth };
+const storage = getStorage(app);
+export { db, auth, storage };
