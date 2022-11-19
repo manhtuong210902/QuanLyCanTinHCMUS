c17b242da16b77a59c1aa92e1447e24e0daa9ccf
20120541 <nhiyenphan99@gmail.com>
2022-11-19 19:55:56 +0700

    .

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,88 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Slider from '../Slider';
 import FoodItem from '../FoodItem';
 import classNames from 'classnames/bind';
 import styles from './Content.module.scss';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { db } from '../../firebase/config';
 const cx = classNames.bind(styles);
 const Content = (props) => {
-    // Get a document, forcing the SDK to fetch from the offline cache.
+    const [foods, serFoods] = useState([]);
 
-    // const [foods, getMenu] = useState([]);
+    useEffect(() => {
+        const unSubscribe = onSnapshot(
+            collection(db, 'foods'),
+            (snapShot) => {
+                let list = [];
+                snapShot.docs.forEach((doc) => {
+                    list.push({ id: doc.id, ...doc.data() });
+                });
+                serFoods(list);
+            },
+            (error) => {
+                console.log(error);
+            },
+        );
 
-    // useEffect(() => {
-    //     console.log("content-render")
-
-    //     let q=query(collection(db,"foods"))
-    //     onSnapshot(q,(snapshot) => {
-    //         let items= snapshot.docs.map((doc) => {
-    //             return {...doc.data()}
-    //         });
-    //         getMenu(items)
-    //     });
-    // }, []);
-
-    const foods = [
-        {
-            name: 'Phở bò',
-            price: 30000,
-            priceImport: 19000,
-            image: '/images/food1.png',
-        },
-        {
-            name: 'Cơm sườn',
-            price: 25000,
-            priceImport: 20000,
-            image: '/images/food2.png',
-        },
-        {
-            name: 'Phở gà',
-            price: 30000,
-            priceImport: 23000,
-            image: '/images/food3.png',
-        },
-        {
-            name: 'Bún bò',
-            price: 30000,
-            priceImport: 21000,
-            image: '/images/food4.png',
-        },
-        {
-            name: 'Nước tăng lực',
-            price: 15000,
-            priceImport: 11000,
-            image: '/images/food5.png',
-        },
-        {
-            name: 'Coca cola',
-            price: 10000,
-            priceImport: 7700,
-            image: '/images/food6.png',
-        },
-        {
-            name: 'String dâu',
-            price: 10000,
-            priceImport: 6500,
-            image: '/images/food7.png',
-        },
-        {
-            name: 'Trà xanh không độ',
-            price: 10000,
-            priceImport: 6000,
-            image: '/images/food8.png',
-        },
-        {
-            name: 'Bánh mì tươi kinh đô',
-            price: 8000,
-            priceImport: 4000,
-            image: '/images/food9.png',
-        },
-        {
-            name: 'Bánh mì tươi Otto',
-            price: 8000,
-            priceImport: 6000,
-            image: '/images/food10.png',
-        },
-    ];
+        return () => {
+            unSubscribe();
+        };
+    }, []);
 
     return (
         <div className={cx('Content')}>
@@ -91,9 +36,7 @@ const Content = (props) => {
                 <h2>Menu</h2>
                 <div className={cx('content-menu-list-item')}>
                     {foods.map((food, index) => (
-                        // <div className={cx('content-menu-item')}>
                         <FoodItem food={food} key={index} handleClick={props.handleClick} />
-                        // </div>
                     ))}
                 </div>
             </div>
--- src/components/FoodItem/index.js
+++ src/components/FoodItem/index.js
@@ -1,26 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import classNames from 'classnames/bind';
 import styles from './FoodItem.module.scss';
 const cx = classNames.bind(styles);
-const index = (props,{handleClick}) => {
+const index = (props, { handleClick }) => {
     const food = props.food;
     return (
         <div className={cx('foodItem')}>
-            <img src={food.image} alt="" />
+            <img src={food.img} alt="" />
             <div className={cx('food-info')}>
                 <label className={cx('food-label')}>{food.name}</label>
                 <div className={cx('food-price')}>
                     Gía :<span>{food.price}đ</span>
                 </div>
                 <div className={cx('btn-add')}>
-                    <button className={cx('food-add')} onClick={()=>props.handleClick(
-                                                                {name:food.name,
-                                                                price:food.price,
-                                                                image:food.image,
-                                                                amount:1
-                                                                })}
-                    >+</button>
+                    <button
+                        className={cx('food-add')}
+                        onClick={() =>
+                            props.handleClick({ name: food.name, price: food.price, image: food.img, amount: 1 })
+                        }
+                    >
+                        +
+                    </button>
                 </div>
             </div>
         </div>
