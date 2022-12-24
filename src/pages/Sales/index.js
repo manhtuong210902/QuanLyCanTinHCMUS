335650bf0c45ba80466e9884854f187bf5ea6372
tun2g <tunghcltt@gmail.com>
2022-12-24 11:26:32 +0700

    add

--- src/pages/Sales/index.js
+++ src/pages/Sales/index.js
@@ -16,6 +16,7 @@ const Sales = () => {
     const [bills, setBills] = useState();
     const [details, setDetails] = useState([]);
     const [type,setType]=useState([0]);
+    const [typeHis,setTypeHis]=useState("yes");
     const [a, setA] = useState(0);
     const user = auth.currentUser;
     let docID = '';
@@ -34,7 +35,6 @@ const Sales = () => {
             collection(db, 'bills'),
             where('userID', '==', user.uid),
             where('orderDate', '==', getCurrentDate()),
-            where('typePament','==',false)
         );
         const querySnapshot = await getDocs(q);
         const data = [];
@@ -91,12 +91,11 @@ const Sales = () => {
         return ('0' + hours).slice(-2) + ':' + ('0' + minutes).slice(-2);
     }
     useEffect(() => {
-        console.log('a');
         getBills().then((data) => {
             setBills(data.data);
             setDetails(data.de);
         });
-    }, [type]);
+    }, [type,typeHis]);
     const clickCheck=async(id)=>{
         const q = query(collection(db, 'bills'), where('billId', '==', id));
         const querySnapshot = await getDocs(q);
@@ -106,7 +105,6 @@ const Sales = () => {
             docID = doc.id;
             bill=doc.data()
         });
-        console.log(bill)
         await updateDoc(doc(db, 'bills', id),{
             typePament:true
         });
@@ -115,10 +113,18 @@ const Sales = () => {
     }
     return (
         <div>
+            <select name="bills" id="bills" value={typeHis} onChange={(e)=>{
+                setTypeHis(e.target.value)
+                console.log(typeHis)
+                }}>
+            <option value="yes" name="bills">Đã nhận món ăn</option>
+            <option value="no" name="bills">Chưa nhận món ăn</option>
+            </select>
             <div className={cx('bill-list')}>
                 <div className={cx('content')}>
+
                     {bills?.reverse().map((bill, index) => {
-                        if(!bill.typePament)
+                        if((typeHis==="no" && !bill.typePament)||(typeHis==="yes"&& bill.typePament))
                         return (
                             <div className={cx('bill-item')}>
                                 <h2 style={{ marginTop: '3px', textAlign: 'center' }}>Hóa đơn</h2>
